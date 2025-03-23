6f492971ae90f40461d4bf4f4fcc9712ffa32ae6
roberty <clsrntkdgh@gmail.com>
2025-03-24 08:35:52 +0900

    테스트 코드

--- IntelliJ/greendae/src/main/java/kr/co/greendae/repository/user/TermsRepository.java
+++ IntelliJ/greendae/src/main/java/kr/co/greendae/repository/user/TermsRepository.java
@@ -1,6 +1,6 @@
 package kr.co.greendae.repository.user;
 
-import kr.co.greendae.entity.Terms.Terms;
+import kr.co.greendae.entity.user.Terms;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TermsRepository extends JpaRepository<Terms, Integer> {
