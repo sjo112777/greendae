70f974a61f6d6eaeef9be4ac4062592910abd6e6
sjo112777 <sjo112777@gmail.com>
2025-03-25 17:15:43 +0900

    v1

--- IntelliJ/greendae/src/main/java/kr/co/greendae/GreendaeApplication.java
+++ IntelliJ/greendae/src/main/java/kr/co/greendae/GreendaeApplication.java
@@ -1,7 +1,10 @@
 package kr.co.greendae;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class GreendaeApplication {
