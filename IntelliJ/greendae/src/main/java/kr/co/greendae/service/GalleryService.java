890172ae9447e3b69972fe61b915982a6d2c943d
unknown <wkdalsgur5556@gmail.com>
2025-04-03 19:41:16 +0900

    갤러리 뷰

--- IntelliJ/greendae/src/main/java/kr/co/greendae/service/GalleryService.java
+++ IntelliJ/greendae/src/main/java/kr/co/greendae/service/GalleryService.java
@@ -1,29 +1,40 @@
 package kr.co.greendae.service;
 
 
+import kr.co.greendae.dto.event.GalleryDTO;
 import kr.co.greendae.entity.event.Gallery;
 import kr.co.greendae.repository.event.GalleryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
+@RequiredArgsConstructor
 @Service
 public class GalleryService {
 
-    private GalleryRepository galleryRepository;
+    private final ModelMapper modelMapper;
+    private final GalleryRepository galleryRepository;
 
-    public GalleryService(GalleryRepository galleryRepository) {
-        this.galleryRepository = galleryRepository;
-    }
 
     public List<Gallery> getAllGalleries(){
         return galleryRepository.findAll();
     }
 
-    public Gallery getGalleryById(int no) {
+    public GalleryDTO getGalleryById(int no) {
         Optional<Gallery> gallery = galleryRepository.findById(no); // Optional로 받기
-        return gallery.orElse(null); // 없으면 null 반환
+
+        // 임시 맛보기용 코드
+
+        GalleryDTO galleryDTO = modelMapper.map(gallery, GalleryDTO.class);
+
+        return galleryDTO;
     }
+
+
 
 }
